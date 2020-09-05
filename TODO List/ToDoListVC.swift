54608c26468c324f60f2c751916328eb29adf04a
Marwan-Sultan <marwansultan97@gmail.com>
2020-09-05 18:40:09 +0200

    Create Add Button with alert and user defaults

--- TODO List/ToDoListVC.swift
+++ TODO List/ToDoListVC.swift
@@ -10,14 +10,19 @@ import UIKit
 
 class ToDoListVC: UITableViewController {
     
-    let itemArray = ["Buy Eggs","Go To the Gym"]
-    
+    var itemArray = ["Buy Eggs","Go To the Gym"]
+    let defaults = UserDefaults.standard
     
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "ToDoList") as? [String] {
+            itemArray = items
+        }
+        
         
     }
 
@@ -48,11 +53,53 @@ class ToDoListVC: UITableViewController {
         
     }
     
+
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let errorLabel = UILabel(frame: CGRect(x: 0, y: 40, width: 270, height:18))
+        errorLabel.textAlignment = .center
+        errorLabel.textColor = .red
+        errorLabel.font = errorLabel.font.withSize(12)
+        errorLabel.isHidden = true
+        
+        
+        let alert = UIAlertController.init(title: "Add New Do Item", message: "", preferredStyle: .alert)
+        alert.view.addSubview(errorLabel)
+        let action = UIAlertAction.init(title: "Add Item", style: .default) { (action) in
+            if textField.text == "" {
+                errorLabel.isHidden = false
+                errorLabel.text = "Please Add Item"
+                 self.present(alert, animated: true, completion: nil)
+            }else {
+                self.itemArray.append(textField.text!)
+                self.defaults.setValue(self.itemArray, forKey: "ToDoList")
+                self.tableView.reloadData()
+            }
+            
+        }
+        
+        
+        
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add Item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true) {
+            alert.view.superview?.isUserInteractionEnabled = true
+            alert.view.superview?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.dismissAlert)))
+        }
+        
+        
+        
+        
+    }
     
-    
-    
-    
-    
+    @objc func dismissAlert() {
+        dismiss(animated: true, completion: nil)
+    }
     
     
     

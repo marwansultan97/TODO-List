f0f8bdd85a71d42eeae463512dfba26dc37dd539
Marwan-Sultan <marwansultan97@gmail.com>
2020-09-10 18:11:07 +0200

    Implement Realm Framework and begin to add data

---
+++ TODO List/Model/Category.swift
@@ -0,0 +1,10 @@
+//
+//  Category.swift
+//  TODO List
+//
+//  Created by Marwan Osama on 9/10/20.
+//  Copyright © 2020 Marwan Osama. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
---
+++ TODO List/Model/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  TODO List
+//
+//  Created by Marwan Osama on 9/10/20.
+//  Copyright © 2020 Marwan Osama. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Item: Object {
+    
+   dynamic var title : String = ""
+   dynamic var done : Bool = false
+    
+    
+    
+}
